1991ebbe8dcd24d58994c43c59fb3f8a83d91e5b
Nanook <56505924+EntenDNA@users.noreply.github.com>
2020-02-20 18:07:48 +0100

    Improved code quality

--- bot_vs_bot.py
+++ bot_vs_bot.py
@@ -1,45 +1,17 @@
-import pygame
 import os
+os.environ.setdefault('PYGAME_HIDE_SUPPORT_PROMPT', 'True')
+
+import pygame
 import random
 import time
+
 pygame.init()
 random.seed()
-
-weis = (255,255,255)
-schwarz = (0,0,0)
-rot = (255,0,0)
-
-humanity = 4 
-
-global frames
-frames = 20
-
-wgröse = 60
-hgröse = 40
-
-gröse = 20
-
-width = wgröse * gröse
-height = hgröse * gröse
-screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Pong")
-
-POKEFONT = pygame.font.Font("PKMN RBYGSC.ttf", 50)
-
-idle = (0,0)
-up = (0,-1)
-down = (0,1)
-directions = ((-1,-1), (1,-1), (-1, 1), (1, 1))
-
-swall = pygame.mixer.Sound("pong_wall.wav")
-splayer = pygame.mixer.Sound("pong1.wav")
-
-firsttime = True
 
 class Bot():
     def __init__(self):
         self.body = [(0,hgröse/2+i) for i in range(5)]
-        self.dir = idle
+        self.dir = Direction.idle
         self.score = 0
 
     def move(self, rc):
@@ -51,13 +23,13 @@ class Bot():
                 self.dir = (0, 1)
             elif zdif < 0:
                 self.dir = (0, -1)
-                
+
             for idx, body in enumerate(self.body):
-                
+
                 x, y = body
                 ay = self.dir[1]
                 nbody = (x, y+ay)
-                
+
                 if ay+y < 0 or self.body[-1][1]+ay > hgröse-1:
                     return
 
@@ -66,12 +38,12 @@ class Bot():
     def draw(self):
             for body in self.body:
                 x, y = body
-                pygame.draw.rect(screen, weis, (x*gröse, y*gröse, gröse, gröse))
+                pygame.draw.rect(screen, Color.weis, (x*gröse, y*gröse, gröse, gröse))
 
 class Bot2(Bot):
     def __init__(self):
         self.body = [(wgröse-1,hgröse/2+z) for z in range(5)]
-        self.dir = idle
+        self.dir = Direction.idle
         self.score = 0
 
     def move(self, rc):
@@ -83,13 +55,13 @@ class Bot2(Bot):
                 self.dir = (0, 1)
             elif zdif < 0:
                 self.dir = (0, -1)
-                
+
             for idx, body in enumerate(self.body):
-                
+
                 x, y = body
                 ay = self.dir[1]
                 nbody = (x, y+ay)
-                
+
                 if ay+y < 0 or self.body[-1][1]+ay > hgröse-1:
                     return
 
@@ -99,38 +71,38 @@ class Ball():
     def __init__(self):
         self.x = int(wgröse/2)
         self.y = int(hgröse/2)
-        self.vector = (1,1)
+        self.vector = Direction.idle
 
     def update_pos(self):
-        if self.vector == idle:
-            self.vector = random.choice(directions)
+        if self.vector == Direction.idle:
+            self.vector = random.choice(Direction.directions)
         self.x += self.vector[0]
         self.y += self.vector[1]
 
         if self.y not in range(1,hgröse-1):
-            swall.play()
-            if self.vector == directions[0]:
-                self.vector = directions[2]
-            elif self.vector == directions[2]:
-                self.vector = directions[0]
+            Sound.swall.play()
+            if self.vector == Direction.directions[0]:
+                self.vector = Direction.directions[2]
+            elif self.vector == Direction.directions[2]:
+                self.vector = Direction.directions[0]
 
-            if self.vector == directions[1]:
-                self.vector = directions[3]
-            elif self.vector == directions[3]:
-                self.vector = directions[1]
+            if self.vector == Direction.directions[1]:
+                self.vector = Direction.directions[3]
+            elif self.vector == Direction.directions[3]:
+                self.vector = Direction.directions[1]
 
         elif self.x not in range(2,wgröse-2):
             if (self.x-1, self.y) in bot.body or (self.x+1, self.y) in bot2.body:
-                splayer.play()
-                if self.vector == directions[3]:
-                    self.vector = directions[2]
-                elif self.vector == directions[2]:
-                    self.vector = directions[3]
+                Sound.splayer.play()
+                if self.vector == Direction.directions[3]:
+                    self.vector = Direction.directions[2]
+                elif self.vector == Direction.directions[2]:
+                    self.vector = Direction.directions[3]
 
-                if self.vector == directions[0]:
-                    self.vector = directions[1]
-                elif self.vector == directions[1]:
-                    self.vector = directions[0]
+                if self.vector == Direction.directions[0]:
+                    self.vector = Direction.directions[1]
+                elif self.vector == Direction.directions[1]:
+                    self.vector = Direction.directions[0]
 
             else:
                 if self.x+self.vector[0] == 0:
@@ -138,35 +110,50 @@ class Ball():
                 else:
                     bot.score += 1
 
-                bot.body = [(0,hgröse/2+i) for i in range(5)]   
+                bot.body = [(0,hgröse/2+i) for i in range(5)]
                 bot2.body = [(wgröse-1,hgröse/2+z) for z in range(5)]
                 self.draw()
                 self.x = int(wgröse/2)
                 self.y = int(hgröse/2)
-                self.vector = idle
+                self.vector = Direction.idle
 
                 return True
 
     def draw(self):
-        pygame.draw.rect(screen, weis, (self.x*gröse, self.y*gröse, gröse, gröse))
+        pygame.draw.rect(screen, Color.weis, (self.x*gröse, self.y*gröse, gröse, gröse))
 
     def display_score(self):
-        pl1 = POKEFONT.render(str(bot.score), False, weis)
-        pl2 = POKEFONT.render(str(bot2.score), False, weis)
+        pl1 = text_font.render(str(bot.score), False, Color.weis)
+        pl2 = text_font.render(str(bot2.score), False, Color.weis)
         screen.blit(pl1,(wgröse/2*gröse-5*gröse-40,hgröse/6*gröse))
         screen.blit(pl2,(wgröse/2*gröse+5*gröse,hgröse/6*gröse))
 
+class Sound():
+    swall = pygame.mixer.Sound("pong_wall.wav")
+    splayer = pygame.mixer.Sound("pong1.wav")
+
+class Color():
+    weis = (255,255,255)
+    schwarz = (0,0,0)
+    rot = (255,0,0)
+
+class Direction():
+    idle = (0,0)
+    up = (0,-1)
+    down = (0,1)
+    directions = ((-1,-1), (1,-1), (-1, 1), (1, 1))
+
 def start_game():
     while True:
-        startt = POKEFONT.render("Insert Coin", False, weis)
+        startt = text_font.render("Insert Coin", False, Color.weis)
         screen.blit(startt,(wgröse/2.75*gröse,hgröse/3*gröse))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 os._exit(1)
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RETURN:
-                    screen.fill(schwarz)
-                    startt = POKEFONT.render("Ready player one", False, weis)
+                    screen.fill(Color.schwarz)
+                    startt = text_font.render("Ready player one", False, Color.weis)
                     screen.blit(startt,(wgröse/3.5*gröse,hgröse/3*gröse))
                     pygame.display.update()
                     pygame.time.wait(2500)
@@ -174,7 +161,7 @@ def start_game():
 
         pygame.display.update()
         pygame.time.wait(500)
-        screen.fill(schwarz)
+        screen.fill(Color.schwarz)
         pygame.display.update()
         pygame.time.wait(500)
 
@@ -184,55 +171,74 @@ def checkwinner():
     elif bot2.score == 10:
         return "Bot two"
 
-bot = Bot()
-bot2 = Bot2()
-ball = Ball()
+if __name__ == "__main__":
+    humanity = 4
 
-clock = pygame.time.Clock()
-oldtime = time.time()
+    wgröse = 60
+    hgröse = 40
 
-while True:
-    while bot.score < 10 and bot2.score < 10:
-        clock.tick(frames)
+    gröse = 20
+
+    firsttime = True
+
+    width = wgröse * gröse
+    height = hgröse * gröse
+    screen = pygame.display.set_mode((width, height))
+    pygame.display.set_caption("Pong")
+
+    text_font = pygame.font.Font("PKMN RBYGSC.ttf", 50)
+
+    bot = Bot()
+    bot2 = Bot2()
+    ball = Ball()
+
+    clock = pygame.time.Clock()
+    frames = 20
+
+    oldtime = time.time()
+
+    while True:
+        while bot.score < 10 and bot2.score < 10:
+            clock.tick(frames)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    os._exit(1)
+
+            screen.fill(Color.schwarz)
+
+            if firsttime:
+                start_game()
+                firsttime = False
+
+            rc = random.randint(1,humanity)
+            rc2 = random.randint(1,humanity)
+            bot.move(rc)
+            bot2.move(rc2)
+            bot.draw()
+            bot2.draw()
+            if ball.update_pos():
+                frames = 20
+                oldtime = time.time()
+
+            ball.draw()
+            ball.display_score()
+
+            if time.time() - oldtime >= 3:
+                oldtime = time.time()
+                frames += 1
+            else:
+                pass
+
+            for i in range(hgröse):
+                pygame.draw.rect(screen, Color.weis, (wgröse/2*gröse, i*gröse, gröse/2, gröse/2))
+
+            pygame.display.flip()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 os._exit(1)
 
-        screen.fill(schwarz)
-
-        if firsttime:
-            start_game()
-            firsttime = False
-
-        rc = random.randint(1,humanity)
-        rc2 = random.randint(1,humanity)
-        bot.move(rc)
-        bot2.move(rc2)
-        bot.draw()
-        bot2.draw()
-        if ball.update_pos():
-            frames = 20
-            oldtime = time.time()
-
-        ball.draw()
-        ball.display_score()
-
-        if time.time() - oldtime >= 3:
-            oldtime = time.time()
-            frames += 1
-        else:
-            pass
-
-        for i in range(hgröse):
-            pygame.draw.rect(screen, weis, (wgröse/2*gröse, i*gröse, gröse/2, gröse/2))
-
-        pygame.display.flip()
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            os._exit(1)
-
-    pygame.draw.rect(screen, schwarz, (wgröse/2*gröse, 0, gröse, hgröse*gröse))
-    winner = POKEFONT.render("{} won!".format(checkwinner()), False, weis)
-    screen.blit(winner,(wgröse/3.5*gröse,hgröse/3*gröse))
-    pygame.display.update()
+        pygame.draw.rect(screen, Color.schwarz, (wgröse/2*gröse, 0, gröse, hgröse*gröse))
+        winner = text_font.render("{} won!".format(checkwinner()), False, Color.weis)
+        screen.blit(winner,(wgröse/3.5*gröse,hgröse/3*gröse))
+        pygame.display.update()
--- pong.py
+++ pong.py
@@ -1,43 +1,17 @@
-import pygame
 import os
+os.environ.setdefault('PYGAME_HIDE_SUPPORT_PROMPT', 'True')
+
+import pygame
 import random
 import time
+
 pygame.init()
 random.seed()
-
-weis = (255,255,255)
-schwarz = (0,0,0)
-rot = (255,0,0)
-
-global frames
-frames = 20
-
-wgröse = 80
-hgröse = 40
-
-gröse = 20
-
-width = wgröse * gröse
-height = hgröse * gröse
-screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Pong")
-
-POKEFONT = pygame.font.Font("PKMN RBYGSC.ttf", 50)
-
-idle = (0,0)
-up = (0,-1)
-down = (0,1)
-directions = ((-1,-1), (1,-1), (-1, 1), (1, 1))
-
-swall = pygame.mixer.Sound("pong_wall.wav")
-splayer = pygame.mixer.Sound("pong1.wav")
-
-firsttime = True
 
 class Player():
     def __init__(self):
         self.body = [(0,hgröse/2+i) for i in range(5)]
-        self.dir = idle
+        self.dir = Direction.idle
         self.score = 0
 
     def move(self):
@@ -55,51 +29,51 @@ class Player():
     def draw(self):
             for body in self.body:
                 x, y = body
-                pygame.draw.rect(screen, weis, (x*gröse, y*gröse, gröse, gröse))
+                pygame.draw.rect(screen, Color.weis, (x*gröse, y*gröse, gröse, gröse))
 
 
 class Player2(Player):
     def __init__(self):
         self.body = [(wgröse-1,hgröse/2+z) for z in range(5)]
-        self.dir = idle
+        self.dir = Direction.idle
         self.score = 0
 
 class Ball():
     def __init__(self):
         self.x = int(wgröse/2)
         self.y = int(hgröse/2)
-        self.vector = (1,1)
+        self.vector = Direction.idle
 
     def update_pos(self):
-        if self.vector == idle:
-            self.vector = random.choice(directions)
+        if self.vector == Direction.idle:
+            self.vector = random.choice(Direction.directions)
         self.x += self.vector[0]
         self.y += self.vector[1]
 
         if self.y not in range(1,hgröse-1):
-            swall.play()
-            if self.vector == directions[0]:
-                self.vector = directions[2]
-            elif self.vector == directions[2]:
-                self.vector = directions[0]
+            Sound.swall.play()
+            if self.vector == Direction.directions[0]:
+                self.vector = Direction.directions[2]
+            elif self.vector == Direction.directions[2]:
+                self.vector = Direction.directions[0]
 
-            if self.vector == directions[1]:
-                self.vector = directions[3]
-            elif self.vector == directions[3]:
-                self.vector = directions[1]
+            if self.vector == Direction.directions[1]:
+                self.vector = Direction.directions[3]
+            elif self.vector == Direction.directions[3]:
+                self.vector = Direction.directions[1]
 
         elif self.x not in range(2,wgröse-2):
             if (self.x-1, self.y) in p1.body or (self.x+1, self.y) in p2.body:
-                splayer.play()
-                if self.vector == directions[3]:
-                    self.vector = directions[2]
-                elif self.vector == directions[2]:
-                    self.vector = directions[3]
+                Sound.splayer.play()
+                if self.vector == Direction.directions[3]:
+                    self.vector = Direction.directions[2]
+                elif self.vector == Direction.directions[2]:
+                    self.vector = Direction.directions[3]
 
-                if self.vector == directions[0]:
-                    self.vector = directions[1]
-                elif self.vector == directions[1]:
-                    self.vector = directions[0]
+                if self.vector == Direction.directions[0]:
+                    self.vector = Direction.directions[1]
+                elif self.vector == Direction.directions[1]:
+                    self.vector = Direction.directions[0]
 
             else:
                 if self.x+self.vector[0] == 0:
@@ -110,30 +84,45 @@ class Ball():
                 self.draw()
                 self.x = int(wgröse/2)
                 self.y = int(hgröse/2)
-                self.vector = idle
+                self.vector = Direction.idle
 
                 return True
 
     def draw(self):
-        pygame.draw.rect(screen, weis, (self.x*gröse, self.y*gröse, gröse, gröse))
+        pygame.draw.rect(screen, Color.weis, (self.x*gröse, self.y*gröse, gröse, gröse))
 
     def display_score(self):
-        pl1 = POKEFONT.render(str(p1.score), False, weis)
-        pl2 = POKEFONT.render(str(p2.score), False, weis)
+        pl1 = text_font.render(str(p1.score), False, Color.weis)
+        pl2 = text_font.render(str(p2.score), False, Color.weis)
         screen.blit(pl1,(wgröse/2*gröse-5*gröse,hgröse/6*gröse))
         screen.blit(pl2,(wgröse/2*gröse+4*gröse,hgröse/6*gröse))
 
+class Sound():
+    swall = pygame.mixer.Sound("pong_wall.wav")
+    splayer = pygame.mixer.Sound("pong1.wav")
+
+class Color():
+    weis = (255,255,255)
+    schwarz = (0,0,0)
+    rot = (255,0,0)
+
+class Direction():
+    idle = (0,0)
+    up = (0,-1)
+    down = (0,1)
+    directions = ((-1,-1), (1,-1), (-1, 1), (1, 1))
+
 def start_game():
     while True:
-        startt = POKEFONT.render("Insert Coin", False, weis)
+        startt = text_font.render("Insert Coin", False, Color.weis)
         screen.blit(startt,(wgröse/2.75*gröse,hgröse/3*gröse))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 os._exit(1)
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RETURN:
-                    screen.fill(schwarz)
-                    startt = POKEFONT.render("Ready player one", False, weis)
+                    screen.fill(Color.schwarz)
+                    startt = text_font.render("Ready player one", False, Color.weis)
                     screen.blit(startt,(wgröse/3.5*gröse,hgröse/3*gröse))
                     pygame.display.update()
                     pygame.time.wait(2500)
@@ -141,7 +130,7 @@ def start_game():
 
         pygame.display.update()
         pygame.time.wait(500)
-        screen.fill(schwarz)
+        screen.fill(Color.schwarz)
         pygame.display.update()
         pygame.time.wait(500)
 
@@ -151,81 +140,97 @@ def checkwinner():
     elif p2.score == 10:
         return "Player two"
 
-p1 = Player()
-p2 = Player2()
-ball = Ball()
+if __name__ == "__main__":
 
-clock = pygame.time.Clock()
-oldtime = time.time()
+    wgröse = 80
+    hgröse = 40
 
-while True:
-    while p1.score < 10 and p2.score < 10:
-        clock.tick(frames)
+    gröse = 20
+
+    width = wgröse * gröse
+    height = hgröse * gröse
+    screen = pygame.display.set_mode((width, height))
+    pygame.display.set_caption("Pong")
+
+    text_font = pygame.font.Font("PKMN RBYGSC.ttf", 50)
+
+    firsttime = True
+
+    p1 = Player()
+    p2 = Player2()
+    ball = Ball()
+
+    clock = pygame.time.Clock()
+    frames = 20
+
+    oldtime = time.time()
+
+    while True:
+        while p1.score < 10 and p2.score < 10:
+            clock.tick(frames)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    os._exit(1)
+
+                elif event.type == pygame.KEYDOWN:
+
+                    if event.key == pygame.K_w:
+                        p1.dir = Direction.up
+
+                    elif event.key == pygame.K_s:
+                        p1.dir = Direction.down
+
+                    elif event.key == pygame.K_UP:
+                        p2.dir = Direction.up
+
+                    elif event.key == pygame.K_DOWN:
+                        p2.dir = Direction.down
+
+                elif event.type == pygame.KEYUP:
+
+                    if event.key == pygame.K_w:
+                        p1.dir = Direction.idle
+
+                    elif event.key == pygame.K_s:
+                        p1.dir = Direction.idle
+
+                    elif event.key == pygame.K_UP:
+                        p2.dir = Direction.idle
+
+                    elif event.key == pygame.K_DOWN:
+                        p2.dir = Direction.idle
+
+            screen.fill(Color.schwarz)
+
+            if firsttime:
+                start_game()
+                firsttime = False
+
+            p1.move()
+            p2.move()
+            p1.draw()
+            p2.draw()
+            if ball.update_pos():
+                frames = 20
+                oldtime = time.time()
+
+            ball.draw()
+            ball.display_score()
+
+            if time.time() - oldtime >= 5:
+                oldtime = time.time()
+                frames += 1
+
+            for i in range(hgröse):
+                pygame.draw.rect(screen, Color.weis, (wgröse/2*gröse, 0+i*gröse, gröse/2, gröse/2))
+
+            pygame.display.update()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 os._exit(1)
 
-            elif event.type == pygame.KEYDOWN:
-
-                if event.key == pygame.K_w:
-                    p1.dir = up
-
-                elif event.key == pygame.K_s:
-                    p1.dir = down
-
-                elif event.key == pygame.K_UP:
-                    p2.dir = up
-
-                elif event.key == pygame.K_DOWN:
-                    p2.dir = down
-
-            elif event.type == pygame.KEYUP:
-
-                if event.key == pygame.K_w:
-                    p1.dir = idle
-
-                elif event.key == pygame.K_s:
-                    p1.dir = idle
-
-                elif event.key == pygame.K_UP:
-                    p2.dir = idle
-
-                elif event.key == pygame.K_DOWN:
-                    p2.dir = idle
-
-        screen.fill(schwarz)
-
-        if firsttime:
-            start_game()
-            firsttime = False
-
-        p1.move()
-        p2.move()
-        p1.draw()
-        p2.draw()
-        if ball.update_pos():
-            frames = 20
-            oldtime = time.time()
-
-        ball.draw()
-        ball.display_score()
-
-        if time.time() - oldtime >= 5:
-            oldtime = time.time()
-            frames += 1
-        else:
-            pass
-
-        for i in range(hgröse):
-            pygame.draw.rect(screen, weis, (wgröse/2*gröse, 0+i*gröse, gröse/2, gröse/2))
-
+        pygame.draw.rect(screen, Color.schwarz, (wgröse/2*gröse, 0, gröse, hgröse*gröse))
+        winner = text_font.render("{} won!".format(checkwinner()), False, Color.weis)
+        screen.blit(winner,(wgröse/3.5*gröse,hgröse/3*gröse))
         pygame.display.update()
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            os._exit(1)
-
-    pygame.draw.rect(screen, schwarz, (wgröse/2*gröse, 0, gröse, hgröse*gröse))
-    winner = POKEFONT.render("{} won!".format(checkwinner()), False, weis)
-    screen.blit(winner,(wgröse/3.5*gröse,hgröse/3*gröse))
-    pygame.display.update()
